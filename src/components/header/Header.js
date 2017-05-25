57d98af56efcf2637aa6b68f4231cb3295f3eafe
galinovivan <9225913@gmail.com>
2017-05-25 15:17:25 +0300

    initial

---
+++ src/components/header/Header.js
@@ -0,0 +1,24 @@
+/**
+ * Created by ivan on 25.05.17.
+ */
+import React, { Component } from 'react';
+import './Header.css';
+
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'colors': '#000'
+        }
+    }
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Header;
